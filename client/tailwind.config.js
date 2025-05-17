e0607dffd222aceaa2a97fc1f72fa38b5677ecee
poirierunited <ale.poirier16@gmail.com>
2025-05-17 13:32:30 -0400

    feat: initialize React project with Vite, ESLint, and Tailwind CSS configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+import flowbitePlugin from "flowbite/plugin";
+import flowbiteAspectRatio from "@tailwindcss/aspect-ratio";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "custom-yellow": "#FBFBE8",
+      },
+    },
+  },
+  plugins: [flowbitePlugin, flowbiteAspectRatio],
+};
