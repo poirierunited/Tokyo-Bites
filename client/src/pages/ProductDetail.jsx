485b812ef7b9bbf46602192088c4d152fb8ac93f
José Reyes <joseareyescona@gmail.com>
2025-05-21 20:54:26 -0400

    Create initial landingpage and product Detail

---
+++ client/src/pages/ProductDetail.jsx
@@ -0,0 +1,198 @@
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import Layout from "../Layouts/Layouts";
+
+// Component to display product details
+function ProductDetail() {
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+  const productReducer = useSelector((state) => state.productReducer);
+  const { loading, error, product } = productReducer;
+
+  const [qty, setQty] = useState(1);
+
+  return (
+    <>
+      <Layout>
+        {loading ? (
+          <h1>cargando...</h1>
+        ) : error ? (
+          <h1>{error}</h1>
+        ) : (
+          <>
+            <section className="text-gray-600 body-font overflow-hidden">
+              <div className="container px-5 py-24 mx-auto bg-yellow-50">
+                <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center items-center">
+                  <div className="lg:w-1/4 w-1/3">
+                    <div className="aspect-h-2 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                      <img
+                        alt="ecommerce"
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                        src={product.image}
+                      />
+                    </div>
+                  </div>
+                  <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                      Precio: {product.price}$
+                    </h2>
+                    <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                      {product.name}
+                    </h1>
+                    <div className="flex mb-4">
+                      <span className="flex items-center">
+                        <svg
+                          fill="currentColor"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-4 h-4 text-indigo-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                        </svg>
+                        <svg
+                          fill="currentColor"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-4 h-4 text-indigo-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                        </svg>
+                        <svg
+                          fill="currentColor"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-4 h-4 text-indigo-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                        </svg>
+                        <svg
+                          fill="currentColor"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-4 h-4 text-indigo-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                        </svg>
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-4 h-4 text-indigo-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                        </svg>
+                        <span className="text-gray-600 ml-3">
+                          {product.numReview} Opiniones
+                        </span>
+                      </span>
+                    </div>
+                    <p className="leading-relaxed">{product.description}</p>
+                    <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                      {/* si cantidad disponible es 0, no mostrar opción de elegir cantidad */}
+
+                      {product.countInStock > 0 ? (
+                        <div className="flex ml-6 items-center">
+                          <span className="mr-3">Cantidad</span>
+                          <div className="relative">
+                            <select
+                              value={qty}
+                              onChange={(e) =>
+                                setQty(parseInt(e.target.value, 10))
+                              }
+                              className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10"
+                            >
+                              {/* setear la cantidad según stock disponible */}
+                              {/* Esto se hará tomando la propiedad 'countInStock' del modelo Producto */}
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                            <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                              <svg
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="w-4 h-4"
+                                viewBox="0 0 24 24"
+                              >
+                                <path d="M6 9l6 6 6-6"></path>
+                              </svg>
+                            </span>
+                          </div>
+                        </div>
+                      ) : (
+                        <>
+                          <h1 className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                            Agotado, intentalo más tarde... ✌️😼{" "}
+                          </h1>
+                        </>
+                      )}
+                    </div>
+                    <div className="flex">
+                      <span className="title-font font-medium text-2xl text-gray-900">
+                        ${product.price}
+                      </span>
+                      {/* boton para agregar al carrito */}
+                      {product.countInStock > 0 ? (
+                        <button
+                          onClick={addToCartHandler}
+                          className="flex ml-auto text-white bg-yellow-400 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-500 rounded"
+                        >
+                          Agregar al carrito de compras
+                        </button>
+                      ) : (
+                        <>
+                          {/* inhabilitar botón */}{" "}
+                          <h1 className="cursor-not-allowed flex ml-auto text-white bg-yellow-400 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-500 rounded">
+                            Sin stock
+                          </h1>
+                        </>
+                      )}
+                      <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                        <svg
+                          fill="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </>
+        )}
+      </Layout>
+    </>
+  );
+}
+
+export default ProductDetail;
